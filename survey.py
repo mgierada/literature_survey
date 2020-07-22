1f8911a43af316d521cd23f530a450096a96debc
Maciej Gierada <maciek.gierada@gmail.com>
2020-07-22 15:42:32 -0700

    added a bit of documentation

--- survey.py
+++ survey.py
@@ -5,7 +5,14 @@ class LiteratureSurvay():
     ''' A class for scraping Google Scholar for a given query '''
 
     def __init__(self, query):
-        ''' Initialize class '''
+        ''' Initialize the class with the user defined options
+
+        Parameters:
+        ___________
+        query : str
+            a phrase to look for - what you want to use to search Google Scholar
+
+        '''
         options = webdriver.ChromeOptions()
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('headless')
@@ -28,7 +35,7 @@ class LiteratureSurvay():
         LiteratureSurvay.get_pdf(self, doi)
 
     def search(self):
-        ''' Click on the first .pdf file to open it '''
+        ''' Searching Google Scholar for a give query '''
         self.driver.get('https://scholar.google.com')
         # find search box and enter query
         self.driver.find_element_by_name('q').send_keys(query)
@@ -47,29 +54,46 @@ class LiteratureSurvay():
 
         '''
         next_pages = []
+        # find id of the page changing bar
         element = self.driver.find_element_by_id('gs_nml')
+        # get links to all pages 2-9
         for el in element.find_elements_by_xpath(
                 ".//a[@class='gs_nma']"):
             next_pages.append(el.get_attribute('href'))
         return next_pages
 
-    def get_links(self):
+    def get_links_to_papers(self):
         ''' Get all links to the research papers 
 
         Returns:
         _______
-        all_links : list(str)
+        all_links_to_papers : list(str)
             a list with links to papers
 
         '''
-        all_links = []
+        all_links_to_papers = []
+        # find class name handling links to the articles
         links = self.driver.find_elements_by_class_name('gs_rt a')
+        # loop through all elements of that class and get links to research articles
         for link in links:
-            all_links.append(link.get_attribute('href'))
-        return all_links
+            all_links_to_papers.append(link.get_attribute('href'))
+        return all_links_to_papers
 
     def get_doi_wiley(self, link_to_paper):
-        ''' Return DOI of the articles published by Wiley '''
+        ''' Return DOI of the articles published by Wiley 
+
+        Parameters:
+        ___________
+
+        link_to_paper : str
+            link to the Wiley article
+            e.g. 'https://onlinelibrary.wiley.com/doi/full/10.1002/jcc.25871'
+
+        Returns:
+        doi : str
+            doi of the paper to be downloaded
+            e.g. 'https://doi.org/10.1002/jcc.25871'
+        '''
         # open page with the article
         self.driver.get(link_to_paper)
         element = self.driver.find_element_by_class_name('epub-doi')
@@ -77,11 +101,20 @@ class LiteratureSurvay():
         return doi
 
     def get_pdf(self, doi):
+        ''' Use Scihub to download .pdf files
+
+        Parameters:
+        ___________
+        doi : str
+            a doi of the paper to be downloaded (with https:/doi.org/)
+            e.g. 'https://doi.org/10.1002/jcc.25871'
+
+        '''
+        # open the scihub page for a given doi
         self.driver.get('https://sci-hub.tw/' + doi)
+        # find 'Save' button element and imitate clicking it
         element = self.driver.find_element_by_id('buttons')
-        for el in element.find_elements_by_xpath(
-                ".//a"):
-            el.click()
+        element.find_elements_by_xpath(".//a")
 
 
 query = 'automation surface reaction mechanism workflow'
