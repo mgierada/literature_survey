586e915e0873c06bf76fd77e3cbfc633697c9309
Maciej Gierada <maciek.gierada@gmail.com>
2020-07-21 17:29:10 -0700

    now all links to the papers are return

--- survey.py
+++ survey.py
@@ -6,9 +6,21 @@ class LiteratureSurvay():
 
     def __init__(self, query):
         ''' Initialize class '''
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('headless')
+        options.add_argument('--test-type')
         DRIVER_PATH = './chromedriver'
+
         self.query = query
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(
+            executable_path=DRIVER_PATH, options=options)
+
+    def run(self):
+        ''' Main run method '''
+        LiteratureSurvay.search(self)
+        links = LiteratureSurvay.get_links(self)
+        print(links)
 
     def search(self):
         ''' Click on the first .pdf file to open it '''
@@ -18,9 +30,24 @@ class LiteratureSurvay():
         # click find button
         self.driver.find_element_by_id('gs_hdr_tsb').click()
         # click the first link with pdf
-        self.driver.find_element_by_class_name('gs_or_ggsm').click()
+        # self.driver.find_element_by_class_name('gs_or_ggsm').click()
+
+    def get_links(self):
+        ''' Get all links to the research papers 
+
+        Returns:
+        _______
+        all_links : list(str)
+            a list with links to papers
+
+        '''
+        all_links = []
+        links = self.driver.find_elements_by_class_name("gs_rt a")
+        for link in links:
+            all_links.append(link.get_attribute('href'))
+        return all_links
 
 
 query = 'automation surface reaction mechanism workflow'
 ls = LiteratureSurvay(query)
-ls.search()
+ls.run()
